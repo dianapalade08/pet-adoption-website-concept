e406d4d90f1a3aa8ab5ecfd4e4446f8355df59e1
Diana Palade <dianapalade08@gmail.com>
2022-06-30 12:09:28 +0300

    Create Pet Adoption Website Concept
    Create Pet Adoption Website Concept

---
+++ app.js
@@ -0,0 +1,16 @@
+var mybutton = document.getElementById("goTopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
